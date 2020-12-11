37d35548a1b4a8378dd12ae6b03bf01fe2ad50e0
Guygolpur <guygolpur@se.shenkar.ac.il>
2020-12-11 21:13:13 +0200

    start working with redux.

--- src/screens/HomePage.js
+++ src/screens/HomePage.js
@@ -1,12 +1,42 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Button from '@material-ui/core/Button'
+import TextField from '@material-ui/core/TextField'
 
-const HomePage = () => (
-    <>
-        <h1>Hello, welcome to my-email!</h1>
-        <p>
-            bla bla bla
-        </p>
-    </>
-)
+
+const HomePage = () => {
+
+    const [receiverEmailAddress, setReceiverEmailAddress] = useState('')
+
+    const loginWithEmail = (event) => {
+        event.preventDefault()
+    }
+
+    return (
+        <div className="page-body">
+            <h1>Welcome</h1>
+            <form onSubmit={loginWithEmail}>
+                <TextField
+                    id="standard-full-width"
+                    label="Please enter an email address:"
+                    style={{ margin: 8 }}
+                    placeholder="example@email.com"
+                    fullWidth
+                    margin="normal"
+                    InputLabelProps={{
+                        shrink: true,
+                    }}
+                    value={receiverEmailAddress} onChange={(event) => setReceiverEmailAddress(event.target.value)}
+                    required={true}
+                    type="email"
+                    variant="outlined"
+                />
+                <Button variant="outlined" color="primary" type="submit" style={{marginLeft: '8px'}}>
+                    Sign in
+                </Button>
+            </form>
+        </div>
+    )
+}
+
 
 export default HomePage
