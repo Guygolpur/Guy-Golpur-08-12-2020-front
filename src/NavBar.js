ab3e693a140f9f1251c7ca3f9f61190079f99690
Guygolpur <guygolpur@se.shenkar.ac.il>
2020-12-12 17:04:36 +0200

    Created log out functionality.

--- src/NavBar.js
+++ src/NavBar.js
@@ -2,7 +2,10 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import Badge from '@material-ui/core/Badge'
 import Avatar from '@material-ui/core/Avatar'
+import Button from '@material-ui/core/Button'
 import { makeStyles, withStyles } from '@material-ui/core/styles'
+import PowerSettingsNewIcon from '@material-ui/icons/PowerSettingsNew'
+
 
 const StyledBadge = withStyles((theme) => ({
     badge: {
@@ -41,9 +44,16 @@ const useStyles = makeStyles((theme) => ({
     },
 }))
 
+const logOut = () => {
+    localStorage.setItem("accountEmailAddress", '')
+    window.location.reload(true)
+}
+
 
 const NavBar = ({ accountEmailAddress }) => {
     const classes = useStyles()
+
+
     return (
         <div style={{ width: '100%', height: '100%', display: 'flex', backgroundColor: '#c7dcff', flexDirection: 'column' }}>
             <div style={{ width: '100%', height: '15%', display: 'flex', justifyContent: "center", alignItems: "center" }}>
@@ -61,25 +71,33 @@ const NavBar = ({ accountEmailAddress }) => {
                 </div>
                 {accountEmailAddress}
             </div>
-            <div style={{ width: '100%', height: '85%', display: 'flex', justifyContent: "center" }}>
+            <div style={{ width: '100%', height: '60%', display: 'flex', justifyContent: "center" }}>
                 <nav>
                     <ul>
-                        <li>
-                            <Link to="/">Home</Link>
-                        </li>
-                        <li>
-                            <Link to="/composeNewEmail">New Email</Link>
-                        </li>
-                        <li>
-                            <Link to="/email-list">Inbox</Link>
-                        </li>
-                        <li>
-                            <Link to="/sent-email-list">Sent</Link>
-                        </li>
+                        {accountEmailAddress ?
+                            <div>
+                                <li>
+                                    <Link to="/composeNewEmail">New Email</Link>
+                                </li>
+                                <li>
+                                    <Link to="/email-list">Inbox</Link>
+                                </li>
+                                <li>
+                                    <Link to="/sent-email-list">Sent</Link>
+                                </li>
+                            </div> :
+                            <div>
+                                <li>
+                                    <Link to="/">Home</Link>
+                                </li>
+                            </div>}
                     </ul>
                 </nav>
             </div>
-
+            <div style={{ width: '100%', height: '10%', display: 'flex', justifyContent: "center" }}>
+                <Button onClick={logOut}><Link to="/"><PowerSettingsNewIcon /></Link></Button>
+            </div>
+            <div style={{ width: '100%', height: '15%', display: 'flex', justifyContent: "center" }} />
         </div>
     )
 }
